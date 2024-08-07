fab484d88faaeeaf99c0254333cf8fc5cf08dffd
virtuoso-04 <anantsharma0404@gmail.com>
2024-08-07 23:02:02 +0530

    yes

---
+++ part A/linearsearch.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class linearsearch {
+
+        public static void main(String[] args) {
+        Scanner ob = new Scanner(System.in);
+        int ele = ob.nextInt();
+        System.out.println("enter the element to be searched");
+        
+    }
+}
